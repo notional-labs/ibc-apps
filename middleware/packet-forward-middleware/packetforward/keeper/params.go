55ffcf52f358ba58f48e3407ecb4f9c14b817322
tungla <anhletung@notional.ventures>
2024-03-27 17:52:52 +0700

    fix: make compatible with forked pfm

--- middleware/packet-forward-middleware/packetforward/keeper/params.go
+++ middleware/packet-forward-middleware/packetforward/keeper/params.go
@@ -4,37 +4,26 @@ import (
 	"github.com/cosmos/ibc-apps/middleware/packet-forward-middleware/v8/packetforward/types"
 
 	sdkmath "cosmossdk.io/math"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-// SetParams sets the module parameters.
+// GetFeePercentage retrieves the fee percentage for forwarded packets from the store.
+func (k Keeper) GetFeePercentage(ctx sdk.Context) sdkmath.LegacyDec {
+	var res sdkmath.LegacyDec
+	k.paramSpace.Get(ctx, types.KeyFeePercentage, &res)
+	return res
+}
+
+// GetParams returns the total set of pfm parameters.
+func (k Keeper) GetParams(ctx sdk.Context) types.Params {
+	return types.NewParams(k.GetFeePercentage(ctx))
+}
+
+// SetParams sets the total set of pfm parameters.
 func (k Keeper) SetParams(ctx sdk.Context, p types.Params) error {
 	if err := p.Validate(); err != nil {
 		return err
 	}
-
-	store := ctx.KVStore(k.storeKey)
-	bz := k.cdc.MustMarshal(&p)
-	store.Set(types.ParamsKey, bz)
+	k.paramSpace.SetParamSet(ctx, &p)
 	return nil
-}
-
-// GetParams returns the current module parameters.
-func (k Keeper) GetParams(ctx sdk.Context) types.Params {
-	var p types.Params
-
-	store := ctx.KVStore(k.storeKey)
-	bz := store.Get(types.ParamsKey)
-	if bz == nil {
-		return p
-	}
-
-	k.cdc.MustUnmarshal(bz, &p)
-	return p
-}
-
-// GetFeePercentage retrieves the fee percentage for forwarded packets from the store.
-func (k Keeper) GetFeePercentage(ctx sdk.Context) sdkmath.LegacyDec {
-	return k.GetParams(ctx).FeePercentage
 }
