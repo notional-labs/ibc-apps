654293260efb4d94cc3c3c928ed80536bd6fe692
Khanh Hoa <hoa@notional.ventures>
2024-05-01 13:09:40 +0700

    fix: get params

--- middleware/packet-forward-middleware/packetforward/keeper/params.go
+++ middleware/packet-forward-middleware/packetforward/keeper/params.go
@@ -15,7 +15,16 @@ func (k Keeper) GetFeePercentage(ctx sdk.Context) sdkmath.LegacyDec {
 
 // GetParams returns the total set of pfm parameters.
 func (k Keeper) GetParams(ctx sdk.Context) types.Params {
-	return types.NewParams(k.GetFeePercentage(ctx))
+	var p types.Params
+
+	store := ctx.KVStore(k.storeKey)
+	bz := store.Get(types.ParamsKey)
+	if bz == nil {
+		return p
+	}
+
+	k.cdc.MustUnmarshal(bz, &p)
+	return p
 }
 
 // SetParams sets the total set of pfm parameters.
